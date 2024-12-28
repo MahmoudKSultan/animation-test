fefd240a847940b8d6a5bbdc7a511f247d22ce57
MahmoudKSultan <mahmoodkns@hotmail.com>
2024-12-29 01:04:57 +0200

    clients

--- src/features/landing-page/Clients.tsx
+++ src/features/landing-page/Clients.tsx
@@ -1,7 +1,27 @@
+"use client";
+import Container from "@/components/Container";
+import { logos } from "@/data/constants/clients-logos";
+import Image from "next/image";
 import React from "react";
+import Marquee from "react-fast-marquee";
 
 export function Clients() {
-  return <div className="h-10 dark:bg-yellow-500">Clients</div>;
+  return (
+    <div className="py-16">
+      <h2 className="text-4xl font-bold mb-10">Clients</h2>
+      <Marquee>
+        {logos.map((logo) => (
+          <Image
+            src={`/assets/images/parteners/${logo}`}
+            width={200}
+            height={100}
+            alt={`${logo}`}
+            key={logo}
+          />
+        ))}
+      </Marquee>
+    </div>
+  );
 }
 
 export default Clients;
--- src/features/landing-page/Hero.tsx
+++ src/features/landing-page/Hero.tsx
@@ -1,8 +1,10 @@
+import Container from "@/components/Container";
+import Image from "next/image";
 import React from "react";
 
 export function Hero() {
   return (
-    <div>
+    <div className="relative mb-48">
       <video
         autoPlay
         loop
@@ -12,8 +14,53 @@ export function Hero() {
       >
         <source src="/assets/hero.mp4" type="video/mp4"></source>
       </video>
+      <VisionMission />
     </div>
   );
 }
+
+const VisionMission = () => {
+  return (
+    <div className="bg-primary md:w-2/3  py-12 md:py-8 mx-auto px-8 text-white relative md:absolute md:-bottom-40 left-1/2 -translate-x-1/2 md:rounded-lg">
+      <Container>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+          {/* Mission Section */}
+          <div className="text-center font-semibold">
+            <div className="flex justify-center">
+              <Image
+                alt="compass image"
+                src={"/assets/images/compass.png"}
+                width={120}
+                height={120}
+                className="w-28 h-28 md:w-20 md:h-20"
+              />
+            </div>
+            <h2 className="text-2xl font-bold mb-2">المهمة</h2>
+            <p className="text-lg">
+              جذب ١٠ مليون سائح سنوياً من الصين إلى السعودية
+            </p>
+          </div>
+
+          {/* Vision Section */}
+          <div className="text-center font-semibold">
+            <div className="flex justify-center">
+              <Image
+                alt="worker image"
+                src={"/assets/images/worker.png"}
+                width={120}
+                height={120}
+                className="w-28 h-28 md:w-20 md:h-20"
+              />
+            </div>
+            <h2 className="text-2xl font-bold mb-2">الرؤية</h2>
+            <p className="text-lg">
+              جعل السعودية وجهة سياحية مفضلة للشعب الصيني
+            </p>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
 
 export default Hero;
