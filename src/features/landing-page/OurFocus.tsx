edef0214c89c5900d4c580a46891901fefd39ddf
Eslam AlBaik <89574402+eslamalbaik@users.noreply.github.com>
2025-01-02 11:35:32 +0200

    commint-ourfoucs-done #2

--- src/features/landing-page/OurFocus.tsx
+++ src/features/landing-page/OurFocus.tsx
@@ -9,41 +9,42 @@ const images = [
     title: "eccommerce",
   },
   {
-    path: "2.png",
+    path: "2.jpeg",
     title: "culture",
   },
   {
-    path: "3.png",
+    path: "3.jpeg",
     title: "terorissm",
   },
   {
-    path: "4.png",
+    path: "4.jpeg",
     title: "media",
   },
 ];
+
 
 function OurFocus() {
   const t = useTranslations("fields");
   return (
     <div className="relative py-16">
       <Container>
-        <h2 className="text-4xl font-bold mb-10">{t("title")}</h2>
+        <h2 className="text-4xl inline-block pb-2 font-bold mb-10 text-primary border-b-4 border-[#1E8022]">{t("title")}</h2>
         <div className="flex flex-wrap -mx-5">
           {images.map((image, idx) => (
             <div
               className="group px-5 mx-auto w-2/3 sm:w-1/2 md:w-1/3 lg:w-1/4 mb-10"
               key={image.title}
             >
-              <div className="relative overflow-hidden">
-                <Image
-                  className="w-full group-hover:scale-125 transition-all duration-300"
-                  width={300}
+      <div className="relative clip-path-cut-corner  overflow-hidden">
+      <Image
+                  className="w-full  min-h-[250px] group-hover:scale-125 transition-all duration-300"
+                  width={400}
                   height={300}
                   alt={`${image.title} image`}
                   src={`/assets/images/our-focus/${image.path}`}
                 />
                 {/* overlay */}
-                <div className="absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-t from-black to-black/10" />
+                <div className="absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-t from-[#1E8022] to-black/10" />
                 <p className="absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white font-bold md:text-xl lg:text-lg">
                   {t(`cols.${idx}`)}
                 </p>
